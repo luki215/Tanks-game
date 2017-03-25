03c80a424c851a670f083b74bed79ee178a4016c
Lukáš Březina <luki.brezina@gmail.com>
2017-03-25 20:02:36 +0100

    Main screen updated

--- zapoctak/MainScreen.h
+++ zapoctak/MainScreen.h
@@ -12,20 +12,21 @@ namespace TanksGame{
 	namespace Screens {
 		class MainScreen : public BasicStaticScreen {
 		protected:	
-			
+			// Inherited via BasicStaticScreen
+			virtual void InitializeComponents() override {
+
+				components.emplace_back(std::make_unique<Components::Buttons::BaseButton>(app_mngr, [&]() {app_mngr.SwitchGameStatusTo(AppStatusEnum::new_game); }, "New game", BasicStructres::SizeAndPos{ 800,500,220,60 }, BasicStructres::Color{ 0x42a500 }, BasicStructres::Color{ 0xffffff }));
+				components.emplace_back(std::make_unique<Components::Buttons::BaseButton>(app_mngr, [&]() {app_mngr.SwitchGameStatusTo(AppStatusEnum::load_game); }, "Load game", BasicStructres::SizeAndPos{ 800,580,220,60 }, BasicStructres::Color{ 0x42a500 }, BasicStructres::Color{ 0xffffff }));
+				components.emplace_back(std::make_unique<Components::Buttons::BaseButton>(app_mngr, [&]() {app_mngr.SwitchGameStatusTo(AppStatusEnum::help); }, "Help", BasicStructres::SizeAndPos{ 800,660,220,60 }, BasicStructres::Color{ 0x42a500 }, BasicStructres::Color{ 0xffffff }));
+
+			}
 			
 		public:
 			MainScreen(TanksGame::TanksAppManager & game_manager) :BasicStaticScreen(game_manager)  {
 				InitializeComponents();
 			}
 
-			// Inherited via BasicStaticScreen
-			virtual void InitializeComponents() override {
-
-				components.emplace_back(std::make_unique<Components::Buttons::BaseButton>(app_mngr, [&]() {app_mngr.SwitchGameStatusTo(AppStatusEnum::game); }, "> Play <", BasicStructres::SizeAndPos{ 800,500,180,50 }, BasicStructres::Color{ 0x42a500 }, BasicStructres::Color{ 0xffffff }));
-				components.emplace_back(std::make_unique<Components::Buttons::BaseButton>(app_mngr, [&]() {app_mngr.SwitchGameStatusTo(AppStatusEnum::help); }, "? Help ?", BasicStructres::SizeAndPos{ 800,570,180,50 }, BasicStructres::Color{ 0x42a500 }, BasicStructres::Color{ 0xffffff }));
-
-			}
+		
 		
 
 		};
